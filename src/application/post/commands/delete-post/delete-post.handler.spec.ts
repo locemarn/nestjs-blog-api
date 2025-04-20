9b3773e6957632a32d53366c91ed158d3a10837f
Marcelo Nogueira <marcelon27@gmail.com>
2025-04-20 19:53:34 -0300

    application layer
    Just removing a ts comment;

--- src/application/post/commands/delete-post/delete-post.handler.spec.ts
+++ src/application/post/commands/delete-post/delete-post.handler.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { DeletePostCommandHandler } from './delete-post.handler';
 import {
